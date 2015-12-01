e1f8864e4e654828c7fb78596f61da0dd7b70b22
resteve <resteve@zikzakmedia.com>
2015-12-01 11:05:41 +0100

    Tests

--- tests/test_commission_user.py
+++ tests/test_commission_user.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the commission_user module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CommissionUserTestCase(unittest.TestCase):
+class CommissionUserTestCase(ModuleTestCase):
     'Test Commission User module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('commission_user')
-
-    def test0005views(self):
-        'Test views'
-        test_view('commission_user')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'commission_user'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CommissionUserTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
